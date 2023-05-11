16a8079b3f20bd423e4a08f7f6b894c8a8feba25
Soraya-j <126451508+Soraya-j@users.noreply.github.com>
2023-05-11 03:00:56 +0200

    ajout théorie des jeux.py

--- IA.py
+++ IA.py
@@ -1,7 +1,5 @@
 import socket
-import sys
 import json
-import re
 import random
 
 # def turn_tile(tile):
@@ -11,16 +9,26 @@ import random
 #     res["S"] = tile["W"]
 #     res["W"] = tile["N"]
 #     return res
-
+coord = {
+    (0,0):0, (0,1):1, (0,2):2, (0,3):3, (0,4):4, (0,5):5, (0,6):6, 
+    (1,0):7, (1,1):8, (1,2):9, (1,3):10, (1,4):11, (1,5):12, (1,6):13,
+    (2,0):14, (2,1):15, (2,2):16, (2,3):17, (2,4):18, (2,5):19, (2,6):20,
+    (3,0):21, (3,1):22, (3,2):23, (3,3):24, (3,4):25, (3,5):26, (3,6):27,
+    (4,0):28, (4,1):29, (4,2):30, (4,3):31, (4,4):32, (4,5):33, (4,6):34,
+    (5,0):35, (5,1):36, (5,2):37, (5,3):38, (5,4):39, (5,5):40, (5,6):41,
+    (6,0):42, (6,1):43, (6,2):44, (6,3):45, (6,4):46, (6,5):47, (6,6):48,   
+}
+list_of_coord = list(coord.keys())
+#print(len(list_of_coord))
 direction = {
-    "N": {"coords": (-1, 0), "inc": -7, "opposite": "S"},
-    "S": {"coords": (1, 0), "inc": 7, "opposite": "N"},
-    "W": {"coords": (0, -1), "inc": -1, "opposite": "E"},
-    "E": {"coords": (0, 1), "inc": 1, "opposite": "W"},
-    (-1, 0): {"name": "N"},
-    (1, 0): {"name": "S"},
-    (0, -1): {"name": "W"},
-    (0, 1): {"name": "E"},
+    "N": {"coords": (0, -1), "inc": -7, "opposite": "S"},
+    "S": {"coords": (0, 1), "inc": 7, "opposite": "N"},
+    "W": {"coords": (-1, 0), "inc": -1, "opposite": "E"},
+    "E": {"coords": (1, 0), "inc": 1, "opposite": "W"},
+    (0,-1): {"name": "N"},
+    (0,1): {"name": "S"},
+    (-1,0): {"name": "W"},
+    (1,0): {"name": "E"},
 }
 dico_num_tuile = {
     'N': [0,1,2,3,4,5],
@@ -28,7 +36,8 @@ dico_num_tuile = {
     'S': [43,44,45,46,47,48],
     'W': [7,14,21,28,35,42],
     'R': [8,9,10,11,12,15,16,17,18,19,22,23,24,25,26,29,30,31,32,33,36,37,38,39,40]
-}Gates = {
+}
+Gates = {
     "V1" : [1, 8, 15, 22 , 29, 36, 43],
     "V2" : [3, 10, 17, 24, 31, 38, 45],
     "V3" : [5, 12, 19, 26, 33, 40, 47],
@@ -36,6 +45,19 @@ dico_num_tuile = {
     "H2" : [21, 22, 23, 24, 25, 26, 27],
     "H3" : [35, 36, 37, 38, 39, 40, 41]
         }
+class Queue:
+	def __init__(self):
+		self.data = []
+
+	def enqueue(self, value):
+		self.data.append(value)
+
+	def dequeue(self):
+		return self.data.pop(0)
+
+	def isEmpty(self):
+		return len(self.data) == 0
+
 class Inscription :
     def __init__(self):
         self.servAdd = ('localhost',3000)
@@ -55,6 +77,7 @@ class Inscription :
     def pong(self):       
         pong = self.transfo({"response": "pong"})
         return pong
+
 class Game:
     def resp_move(self):
         move = i.transfo({"response": "move", "move": self.move_played(), "message": "I played"})
@@ -87,6 +110,7 @@ class Game:
     def move_played(self):
         gates = self.prob_gates()
         letter = gates[random.randint(0,len(gates)-1)]
+        print('letter : ',letter)
         the_move_played = {"tile": self.free_tile(), "gate": letter , "new_position": self.new_pos()}   
         return the_move_played
     def free_tile(self):              
@@ -95,7 +119,10 @@ class Game:
         print(tile)
         return tile
     def new_pos(self):
-        path = self.path() 
+        print('fonction bfs:::')
+        self.BFS(list_of_coord[self.actual_pos()], self.my_target())    
+        print('test tile : ', self.new_tile(self.actual_pos()))
+        path = self.path(self.new_tile(self.actual_pos()))
         if path == []:
             add = 0
         else :
@@ -113,22 +140,20 @@ class Game:
         current_pos = receipt['state']['positions']
         actual = current_pos[int(current)]
         return actual
-    def path(self):
-        tile = self.new_tile()
-        print('test tile :')
-        print(tile)
+    def path(self, tile):
+        #tile = self.new_tile(self.actual_pos())
+        # print('test tile :')
+        # print(tile)
         cardinal = ['N', 'E', 'S', 'W']
         path = []
         for elem in cardinal:
             if tile[elem] == True:
-                print(tile[elem])
                 path.append(direction[elem]['inc'])                
         print('path:')
         print(path)
         return path
-    def new_tile(self):
+    def new_tile(self, act_pos):
         new_tile = {}
-        act_pos = int(self.actual_pos())
         board = receipt['state']['board']
         if act_pos in dico_num_tuile['N'] :
             new_tile['N'] = False
@@ -185,19 +210,59 @@ class Game:
             return new_tile
     def my_target(self):
         target = receipt['state']['target']
+        res = []
         print('my target :')
         print(target)
         i = 0
         board = receipt['state']['board']
-        print('len_board : ',len(board))
         while i < len(board) - 1 :
             i += 1
             if receipt['state']['board'][i]['item'] == target :
                 pos_tuile_target = i
                 print('my pos : ', self.actual_pos())
                 print('pos_tuile_target : ', pos_tuile_target)
+                res.append(pos_tuile_target)
                 break 
-                return pos_tuile_target
+        return res
+    
+    def BFS(self,start, pos_target):
+        q = Queue()
+        q.enqueue(start)
+        while not q.isEmpty():
+            node = q.dequeue()
+            if node in pos_target:
+                print("J'AI TROUVÉ UN TRÉSOR")
+            for elem in self.successors(node):
+                    q.enqueue(elem)
+            node = None
+
+        res = []
+        while node is not None:
+            res.append(node)
+            print('liste du chemin : ',list(reversed(res)))
+        print('liste du chemin : ',list(reversed(res)))
+
+    def successors(self, node):
+        directions = [direction['N']['coords'], direction['E']['coords'], direction['S']['coords'], direction['W']['coords']]
+        res = []
+        board = receipt['state']['board']
+        x, y = node
+        for dx, dy in directions:
+            nx = x + dx
+            ny = y + dy
+            try :
+                match_tuile = coord[(nx,ny)]
+                dir = direction[(dx,dy)]['name']
+                if board[match_tuile][dir] and board[match_tuile + direction[dir]['inc']][direction[dir]['opposite']]:                   
+                        res.append((nx, ny))
+            except KeyError:
+                print('error_out of the board : ',(nx,ny))
+                dx = None
+                dy = None
+        print('res : ',res)
+        return res
+    
+
 
 
 i = Inscription()
@@ -217,7 +282,9 @@ while True:
     if receipt['request'] == 'play':
         print(receipt['request'])
         c.send(g.resp_move())
+        g.my_target()
+        print('errors :')
+        print(receipt['errors'])
            
         
 c.close()
-
